793e66c12b3a8fc349c66e774a79671368509325
Raphael-Zak <raphael.zakrzewski@gmail.com>
2025-04-25 00:07:33 +0200

    Delete Flandres_azote directory

--- Flandres_azote/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Paramètres SMTP sécurisés
-    define('SMTP_HOST', 'ns1.o2switch.net');
-    define('SMTP_USER', 'contact@flandresazote.fr');
-    define('SMTP_PASS', 'Le@Emm@Theo2022'); // ⚠️ Modifier et stocker de manière sécurisée !
-    define('SMTP_PORT', 587);
-?>
