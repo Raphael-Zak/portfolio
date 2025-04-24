f6fbd23c3c017b703d7df6df60a108c98005d345
Raphael-Zak <raphael.zakrzewski@gmail.com>
2025-04-24 23:46:43 +0200

    Add files via upload

---
+++ Flandres_azote/public/require/footer.php
@@ -0,0 +1,77 @@
+<div class="container-footer">
+    <div class="logo">
+        <div class="colonne">
+            <a href="index.php" class="logo">
+                <img src="./img/Flandres_Azote_logo.png" alt="Logo / Redirection accueil">
+            </a>
+            <p>Vous pouvez nous contactez les jours ouvrés de ..h à ..h</p>
+        </div>
+    </div>
+
+    <div class="container-colonne">
+        <div class="colonne">
+            <h1>Offres et Services</h1>
+
+            <a href="./livraison.php"><span>Livraison</span></a>
+
+            <a href="./materiels.php"><span>Matériel Cryogénique</span></a>
+        </div>
+
+        <div class="colonne">
+            <h1>Nos marchés</h1>
+
+            <a href=""><span>Stockage cryogenique</span></a>
+
+            <a href="#dermatologie"><span>Dermatologie</span></a>
+
+            <a href=""><span>Cryothérapie</span></a>
+
+            <a href=""><span>Overclocking</span></a>
+        </div>
+
+        <div class="colonne">
+            <h1>Contact</h1>
+            <div class="container-contact">
+                <img src="./img/phone_icon_blanc.png" alt="Téléphone ">
+                <p>03 74 77 00 02</p>
+            </div>
+            <div class="container-contact">
+                <img src="./img/fax_icon_blanc.png" alt="Fax ">
+                <p>03 74 77 00 01</p>
+            </div>
+            <div class="container-contact">
+                <img src="./img/mail_icon_blanc.png" alt="Mail ">
+                <p>contact@flandresazote.fr</p>
+            </div>
+            <div class="container-contact">
+                <img src="./img/localisation_icon_blanc.png" alt="Courrier ">
+                <p>40 Rue de Bazuel – 59360 POMMEREUIL</p>
+            </div>
+            <div class="container-contact">
+                <a href="./Formulaire_contact.php">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="20"
+                        height="20"
+                    >
+                        <path fill="none" d="M0 0h24v24H0z"></path>
+                        <path
+                        fill="currentColor"
+                        d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                        ></path>
+                    </svg>
+                    <span>Formulaire de contact</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="legal">
+    <p>© 2025 Flandres Azote </p>
+    <p>|</p>
+    <a href="./confidentialite.php">Confidentialité</a>
+    <p>|</p>
+    <a href="./mentions-legales.php">Mentions légales</a>
+</div>
